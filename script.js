1b655ad0c2610b54bf3bbb62a517e0870929de56
Mrgaton <arnaupost4@gmail.com>
2024-04-20 23:03:34 +0200

    ee

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Mrgaton youtube downloader
 // @namespace    http://tampermonkey.net/
-// @version      2024-04-20
+// @version      2024-04-22
 // @description  Download using crun and my awesome program
 // @author       Mrghaton
 // @match        https://www.youtube.com/watch?v=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @updateURL    https://raw.githubusercontent.com/Mrgaton/YoutuveDownloader/master/script.js
+// @downloadURL  https://raw.githubusercontent.com/Mrgaton/YoutuveDownloader/master/script.js
 // @grant        none
 // ==/UserScript==
 
