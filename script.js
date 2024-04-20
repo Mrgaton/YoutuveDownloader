854d4d3a219b059d2937ff7206ad1f1ab2b573a6
Mrgaton <arnaupost4@gmail.com>
2024-04-20 20:49:13 +0200

    update script.js aja

--- script.js
+++ script.js
@@ -6,6 +6,7 @@
 // @author       Mrghaton
 // @match        https://www.youtube.com/watch?v=*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @downloadURL  https://github.com/Mrgaton/YoutuveDownloader/raw/master/script.js
 // @grant        none
 // ==/UserScript==
 
