d0b53e9bea51ac5f5abf2f83d20633521e3d7165
Mrgaton <arnaupost4@gmail.com>
2024-04-24 20:07:21 +0200

    update script.user.js

--- script.user.js
+++ script.user.js
@@ -20,21 +20,36 @@ const resources = [
 	'https://raw.githubusercontent.com/Mrgaton/YoutuveDownloader/master/script.core.js'
 ];
 
+String.prototype.hashCode = function () {
+	var hash = 0,
+		i,
+		chr;
+	if (this.length === 0) return hash;
+	for (i = 0; i < this.length; i++) {
+		chr = this.charCodeAt(i);
+		hash = (hash << 5) - hash + chr;
+		hash |= 0; // Convert to 32bit integer
+	}
+	return hash;
+};
+
 (async function () {
 	resources.forEach((url) => {
+		let urlHash = url.hashCode();
+
 		GM.xmlHttpRequest({
 			url: url,
 			onload: async (response) => {
 				const text = response.responseText;
-				const storageData = await GM.getValue('CachedScriptKey');
+				const storageData = await GM.getValue(urlHash);
 
 				if (text != storageData) {
 					console.log('reload!');
 
-					await GM.setValue('CachedScriptKey', text);
+					await GM.setValue(urlHash, text);
 					location.reload();
 				} else {
-					console.log('NO reload!');
+					console.log('Resource already updated!');
 				}
 			}
 		});
